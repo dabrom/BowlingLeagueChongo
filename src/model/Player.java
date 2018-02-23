32a27aa20afdc246d0a27f3c086141da8bb21b4e
meyerCodeUniverse <36606751+meyerCodeUniverse@users.noreply.github.com>
2018-02-23 15:23:53 -0600

    Added Player class

---
+++ src/model/Player.java
@@ -0,0 +1,88 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "player")
+
+public class Player {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String screenName;
+	@ManyToOne
+	@JoinColumn(name = "teamId")
+	private Team team;
+	
+	
+	
+	public Player() {
+		super();
+	}
+
+	public Player(String firstName, String lastName, String phoneNumber, String screenName, Team team) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.screenName = screenName;
+		this.team = team;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getScreenName() {
+		return screenName;
+	}
+
+	public void setScreenName(String screenName) {
+		this.screenName = screenName;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber=" + phoneNumber
+				+ ", screenName=" + screenName + ", team=" + team + "]";
+	}
+	
+	
+	
+}
